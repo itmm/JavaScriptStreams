6fbaaa79ee4e87376894702e298f493c53926e05
Timm Knape <timm.knape@dvag.com>
2014-12-30 17:06:05 +0100

    initial commit

---
+++ jss.js
@@ -0,0 +1,22 @@
+/**
+ * JavaScript Streams (jss), imitating Scheme Streams in JavaScript
+ */
+ 
+function stream_cons(a, b) { return { car: a, cdr: b}; }
+function stream_car(s) { return s.car; }
+function stream_cdr(s) { return s.cdr(); }
+
+function stream_map(a, b, f) {
+	return stream_cons(f(stream_car(a), stream_car(b)), function () {
+		return stream_map(stream_cdr(a), stream_cdr(b), f);
+	});
+	
+}
+function stream_add(a, b) { return stream_map(a, b, function(a, b) { return a + b; }); }
+
+function stream_filter(s, p) {
+	while (!p(stream_car(s))) { s = stream_cdr(s); }
+	return stream_cons(stream_car(s), function() {
+		return stream_filter(stream_cdr(s), p);
+	});
+}
---
+++ primes.js
@@ -0,0 +1,32 @@
+function make_ones() { return stream_cons(1, make_ones) }
+
+function make_integers(from) {
+	return stream_cons(from, function() {
+		return stream_add(make_ones(), make_integers(from));
+	});
+}
+
+function filter_primes(s) {
+	var num = stream_car(s);
+	return stream_cons(num, function() {
+		return filter_primes(stream_filter(s, function(v) { return v % num; }));
+	});
+}
+
+var primes = filter_primes(make_integers(2));
+var result = document.getElementById("result");
+
+function cell(content) {
+	var cell = document.createElement("td");
+	cell.appendChild(document.createTextNode(content));
+	return cell;
+}
+
+for (var i = 1; i <= 20; ++i) {
+	var row = document.createElement("tr");
+	row.appendChild(cell("pi(" + i + ")"));
+	row.appendChild(cell("=="));
+	row.appendChild(cell("" + stream_car(primes)));
+	result.appendChild(row);
+	primes = stream_cdr(primes);
+}		
